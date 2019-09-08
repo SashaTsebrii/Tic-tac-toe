9992de49c677fa44e3304f50866d3985c9ce73e2
SashaTsebrii <sashatsebrii@gmail.com>
2019-09-08 18:12:09 +0300

    Initial Commit

---
+++ Tic-tac-toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-tac-toe
+//
+//  Created by Aleksandr Tsebrii on 9/8/19.
+//  Copyright © 2019 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
